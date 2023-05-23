85f2b3ed0d6a27e4edf872a7ee1f19344a0b320f
alexberkut98 <56170571+alexberkut98@users.noreply.github.com>
2023-05-23 17:22:12 +0300

    Update make_prediction.py

--- lab2/make_prediction.py
+++ lab2/make_prediction.py
@@ -4,7 +4,7 @@ import pandas as pd
 loaded_model = pickle.load(open('/model.pkl', 'rb'))
 
 # прочитаем из csv файла подготовленный датасет для обучения
-data_test = pd.read_csv('/home/alexberkut98/ubuntu/project/data_test.csv')
+data_test = pd.read_csv('/data_test.csv')
 X_test = data_test[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch']].values
 
 # сделаем предсказание для первого пассажира из тестовой выборки
