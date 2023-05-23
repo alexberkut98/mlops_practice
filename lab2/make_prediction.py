d1adcfec997cbcf77ff6d444f00e9cf5f746266e
alexberkut98 <56170571+alexberkut98@users.noreply.github.com>
2023-05-23 17:12:01 +0300

    Update make_prediction.py

--- lab2/make_prediction.py
+++ lab2/make_prediction.py
@@ -1,7 +1,7 @@
 import pickle
 import pandas as pd
 
-loaded_model = pickle.load(open('/home/alexberkut98/ubuntu/project/model.pkl', 'rb'))
+loaded_model = pickle.load(open('/model.pkl', 'rb'))
 
 # прочитаем из csv файла подготовленный датасет для обучения
 data_test = pd.read_csv('/home/alexberkut98/ubuntu/project/data_test.csv')
